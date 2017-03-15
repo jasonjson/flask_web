2b17454923933b92de7c54f3d7e211e52fae64ad
jason <johnny.lyy@gmail.com>
2017-03-14 20:38:26 -0400

    formatting

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 from flask_login import LoginManager
-from flask_openid import OpenID
-from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,5 +8,5 @@ db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
 from app import views, models
